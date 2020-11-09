8f9e1f6a0e93e824503bf539b6d954d4f80c4766
Txart <iurzainki@gmail.com>
2020-11-09 11:51:27 +0200

    contains in fortran

--- finite-diff.f95
+++ finite-diff.f95
@@ -133,15 +133,19 @@ subroutine j_and_f(N, v, v_old, b, delta_t, delta_x, diri_bc, s1, s2, t1, t2, so
     e = 1/(2*delta_x**2)
 
     do i=2,N
-        J(i,i-1) = e*(-dif_prime(v(i-1), b(i-1))*v(i-1) - dif(v(i), b(i)) - dif(v(i-1), b(i-1)) + dif_prime(v(i-1), b(i-1))*v(i))
+        J(i,i-1) = e*(-dif_prime(v(i-1), b(i-1))*v(i-1) - dif(v(i), b(i)) - dif(v(i-1), b(i-1)) + & 
+						dif_prime(v(i-1), b(i-1))*v(i))
 
-        J(i,i) = e*(-dif_prime(v(i), b(i))*v(i-1) + 2*dif_prime(v(i), b(i))*v(i) - dif_prime(v(i), b(i))*v(i+1) + dif(v(i+1), b(i+1)) & 
+        J(i,i) = e*(-dif_prime(v(i), b(i))*v(i-1) + 2*dif_prime(v(i), b(i))*v(i) - & 
+					dif_prime(v(i), b(i))*v(i+1) + dif(v(i+1), b(i+1)) & 
                         + 2*dif(v(i), b(i)) + dif(v(i-1), b(i-1))) + 1/delta_t
 
-        J(i,i+1) = e*(dif_prime(v(i+1), b(i+1))*v(i) - dif(v(i+1), b(i+1)) - dif(v(i), b(i)) - dif_prime(v(i+1), b(i+1))*v(i+1))
+        J(i,i+1) = e*(dif_prime(v(i+1), b(i+1))*v(i) - dif(v(i+1), b(i+1)) - dif(v(i), b(i)) &
+						- dif_prime(v(i+1), b(i+1))*v(i+1))
 
         ! F
-        F(i) = -e*((dif(v(i), b(i)) + dif(v(i-1), b(i-1)))*v(i-1) -v(i)*(dif(v(i+1), b(i+1)) + 2*dif(v(i), b(i)) + dif(v(i-1), b(i-1))) &
+        F(i) = -e*((dif(v(i), b(i)) + dif(v(i-1), b(i-1)))*v(i-1) -v(i)*(dif(v(i+1), b(i+1)) & 
+							+ 2*dif(v(i), b(i)) + dif(v(i-1), b(i-1))) &
                        + v(i+1)*(dif(v(i+1), b(i+1)) + dif(v(i), b(i)))) - source - v_old(i)/delta_t + v(i)/delta_t
 
     end do
@@ -152,10 +156,13 @@ subroutine j_and_f(N, v, v_old, b, delta_t, delta_x, diri_bc, s1, s2, t1, t2, so
     F(1) = diri_bc
     ! Neumann with diffusivity(u(L))*u'(L)=0 in x=N
     aL = dif(v(N), b(N))
-    J(N+1,N+1) = e*(-dif_prime(v(N), b(N))*v(N) + 2*dif_prime(v(N+1), b(N+1))*v(N+1) + aL + 2*dif(v(N+1), b(N+1)) + dif(v(N), b(N))) + 1/delta_t
-    J(N+1,N) = e*(-dif_prime(v(N), b(N))*v(N) + dif_prime(v(N), b(N))*v(N+1) - aL - 2*dif(v(N+1), b(N+1)) - dif(v(N), b(N))) -delta_x*e*(dif_prime(v(N+1), b(N+1)))
-    F(N+1) = -e*((dif(v(N+1), b(N+1)) + dif(v(N), b(N)))*v(N) -v(N+1)*(aL + 2*dif(v(N+1), b(N+1)) + dif(v(N), b(N))) &
-                      + v(N)*(aL + dif(v(N+1), b(N+1)))) - source - v_old(N+1)/delta_t + v(N+1)/delta_t
+    J(N+1,N+1) = e*(-dif_prime(v(N), b(N))*v(N) + 2*dif_prime(v(N+1), b(N+1))*v(N+1) &
+					+ aL + 2*dif(v(N+1), b(N+1)) + dif(v(N), b(N))) + 1/delta_t
+    J(N+1,N) = e*(-dif_prime(v(N), b(N))*v(N) + dif_prime(v(N), b(N))*v(N+1) - aL &
+					- 2*dif(v(N+1), b(N+1)) - dif(v(N), b(N))) -delta_x*e*(dif_prime(v(N+1), b(N+1)))
+    F(N+1) = -e*((dif(v(N+1), b(N+1)) + dif(v(N), b(N)))*v(N) -v(N+1)*(aL + 2*dif(v(N+1), b(N+1)) + & 
+						dif(v(N), b(N)))+ v(N)*(aL + dif(v(N+1), b(N+1)))) &
+						- source - v_old(N+1)/delta_t + v(N+1)/delta_t
 
 	contains ! Add subroutines in here in order to share parameters
 		function dif(x,bi) result(y)
